77265308826a2c030d4580286d1f1bd8adc378db
Seun Oyediran <seunoyediran22517@gmail.com>
2024-03-06 20:25:23 +0100

    get random number added

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ export function joinName(firstName = "", lastName = "") {
   return `${firstName} ${lastName}`;
 }
 
+export function getRandomNumber(limit) {
+  const randomIndex = Math.floor(Math.random() * limit);
+
+  return randomIndex;
+}
+
 const utils = {
   joinName,
+  getRandomNumber,
 };
 
 module.exports = utils;
